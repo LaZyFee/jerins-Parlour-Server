9a438f5176d85fcf72b434b1874df5ad977880b3
Rabiul Hasan Rafee <rhr277@gmail.com>
2025-01-20 16:08:01 +0600

    db

--- src/Config/connectDB.js
+++ src/Config/connectDB.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
-        console.log("MongoDB Connected");
+        const conn = await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log(error);
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
-};
+}; 
