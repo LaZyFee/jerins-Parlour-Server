ca496f47d69527380ee96f97d82f060ed572711f
Rabiul Hasan Rafee <rhr277@gmail.com>
2025-01-21 11:21:22 +0600

    vercel.json updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { loadEnv } from "vite";
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    base: "/client/", // Set the base path for assets
+    base: "/", // Set base to root
     server: {
       proxy: {
         "/api": {
